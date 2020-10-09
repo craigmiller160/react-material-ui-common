467558b9f50203acd5343d4892a60899779c935f
Craig Miller <craigmiller160@gmail.com>
2020-10-09 15:46:28 -0400

    Fixed IDs

--- src/Navbar/MobileMenu.tsx
+++ src/Navbar/MobileMenu.tsx
@@ -85,7 +85,7 @@ const MobileMenu = (props: Props) => {
 
                     return (
                         <ListItem
-                            id={ `navbar-mobile-item-${item.to}` }
+                            id={ `navbar-mobile-item-${item.to.replaceAll('/', '-')}` }
                             key={ index }
                             className={ itemClass }
                             onClick={ props.handleMenuClose }
