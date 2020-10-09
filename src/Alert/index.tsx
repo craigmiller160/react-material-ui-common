c0b0d8ef39d2956fd87d4964d0a52202530d4a4b
Craig Miller <craigmiller160@gmail.com>
2020-10-08 21:17:43 -0400

    Migrated redux alert

---
+++ src/Alert/index.tsx
@@ -0,0 +1,51 @@
+/*
+ * material-ui-common
+ * Copyright (C) 2020 Craig Miller
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import MuiAlert from '@material-ui/lab/Alert';
+import alertSlice, { AlertState } from './slice';
+import AlertTitle from '@material-ui/lab/AlertTitle';
+import Collapse from '@material-ui/core/Collapse';
+
+interface RootState {
+    alert: AlertState
+}
+
+const capitalize = (text: string) => {
+    const firstLetter = text.substring(0, 1).toUpperCase();
+    return `${firstLetter}${text.substring(1)}`;
+};
+
+const Alert = () => {
+    const dispatch = useDispatch();
+    const alertState = useSelector<RootState,AlertState>((state) => state.alert, shallowEqual);
+    return (
+        <Collapse in={ alertState.show }>
+            <MuiAlert
+                severity={ alertState.type }
+                onClose={ () => dispatch(alertSlice.actions.hideAlert()) }
+            >
+                <AlertTitle>{ capitalize(alertState.type) }</AlertTitle>
+                { alertState.message }
+            </MuiAlert>
+        </Collapse>
+    );
+};
+
+export default Alert;
---
+++ src/Alert/slice.ts
@@ -0,0 +1,57 @@
+/*
+ * material-ui-common
+ * Copyright (C) 2020 Craig Miller
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export interface AlertState {
+    show: boolean;
+    type: 'success' | 'error' | 'warning' | 'info';
+    message: string;
+}
+
+const initialState: AlertState = {
+    show: false,
+    message: '',
+    type: 'success'
+};
+
+const showErrorAlert = (state: AlertState, action: PayloadAction<string>) => {
+    state.show = true;
+    state.type = 'error';
+    state.message = action.payload;
+};
+
+const showSuccessAlert = (state: AlertState, action: PayloadAction<string>) => {
+    state.show = true;
+    state.type = 'success';
+    state.message = action.payload;
+};
+
+const hideAlert = (state: AlertState) => {
+    state.show = false;
+};
+
+export default createSlice({
+    name: 'alert',
+    initialState,
+    reducers: {
+        showErrorAlert,
+        showSuccessAlert,
+        hideAlert
+    }
+});
