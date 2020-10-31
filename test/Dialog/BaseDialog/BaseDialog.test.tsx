e3e755d059aff1f324a752fb4b2186ee05bee245
Craig Miller <craigmiller160@gmail.com>
2020-10-31 17:30:31 -0400

    Finished BaseDialog tests

--- test/Dialog/BaseDialog/BaseDialog.test.tsx
+++ test/Dialog/BaseDialog/BaseDialog.test.tsx
@@ -16,12 +16,15 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import BaseDialog, { Props as BaseDialogProps } from '../../../src/Dialog/BaseDialog';
+import createTestComponent from '../../utils/createTestComponent';
+import { mount, ReactWrapper } from 'enzyme';
+import renderingValidator, { RenderedItem } from '../../utils/renderingValidator';
 
 const actionClick = jest.fn();
 
-const defaultProps: BaseDialogProps = {
+const defaultProps: PropsWithChildren<BaseDialogProps> = {
     id: 'BaseDialog',
     open: true,
     title: 'Base Dialog',
@@ -34,16 +37,86 @@ const defaultProps: BaseDialogProps = {
     className: 'MyClass'
 };
 
+const TestBaseDialog = createTestComponent(defaultProps, BaseDialog);
+
+const dialogRoot: RenderedItem = {
+    selector: 'WithStyles(ForwardRef(Dialog))',
+    values: [
+        {
+            props: {
+                id: 'BaseDialog',
+                open: true,
+                className: 'BaseDialog MyClass'
+            }
+        }
+    ]
+};
+
+const dialogTitle: RenderedItem = {
+    selector: 'ForwardRef(DialogTitle)',
+    values: [
+        {
+            text: 'Base Dialog'
+        }
+    ]
+};
+
+const dialogContent: RenderedItem = {
+    selector: 'ForwardRef(DialogContent)',
+    values: [{}]
+};
+
+const dialogActions: RenderedItem = {
+    selector: 'ForwardRef(DialogActions) ForwardRef(Button)',
+    values: [
+        {
+            props: {
+                id: 'BaseDialog-btn-0',
+                color: 'primary',
+                onClick: expect.any(Function)
+            }
+        }
+    ]
+};
+
 describe('BaseDialog', () => {
     describe('rendering', () => {
         it('renders dialog', () => {
-            throw new Error();
+            const wrapper: ReactWrapper = mount(
+                <TestBaseDialog>
+                    <p>Hello World</p>
+                </TestBaseDialog>
+            );
+
+            const contentBody: RenderedItem = {
+                selector: 'ForwardRef(DialogContent) p',
+                values: [
+                    {
+                        text: 'Hello World'
+                    }
+                ]
+            };
+
+            const items: Array<RenderedItem> = [
+                dialogRoot,
+                dialogTitle,
+                dialogContent,
+                contentBody,
+                dialogActions
+            ];
+
+            renderingValidator(wrapper, items);
         });
     });
 
     describe('actions', () => {
         it('handles button action', () => {
-            throw new Error();
+            const wrapper: ReactWrapper = mount(
+                <TestBaseDialog />
+            );
+
+            wrapper.find('button#BaseDialog-btn-0').simulate('click');
+            expect(actionClick).toHaveBeenCalled();
         });
     });
 });
